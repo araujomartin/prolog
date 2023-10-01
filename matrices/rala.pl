e40a5a909ea1a54ecbc950003095ebce03c17c58
Martin Araujo <martin.araujo@agripay.ar>
2023-10-01 18:24:38 -0300

    Matrices: matriz rala

---
+++ matrices/rala.pl
@@ -0,0 +1,19 @@
+
+matrizRala(M,MR):-
+    rala(M,MR,1,1).
+
+rala([],[],_,_).
+
+rala([[E|Es]|R],SR,X,Y):-
+    E=0,
+    X1 is X+1,
+    rala([Es|R],SR,X1,Y).
+
+rala([[E|Es]|R],[[X,Y,E]|SR],X,Y):-
+    E\=0,
+    X1 is X+1,
+    rala([Es|R],SR,X1,Y).
+
+rala([[]|R],SR,_,Y):-
+    Y1 is Y+1,
+    rala(R,SR,1,Y1).
